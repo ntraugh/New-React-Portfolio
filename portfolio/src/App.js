3f4f4658bc4bbaacebe0268bbbd96e4b5aa1cd5a
Nate Traugh <n.traugh@yahoo.com>
2022-07-04 21:27:58 -0400

    added the navbar component, need to add hover effects for the images later

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import "./index.css";
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from './components/Navbar';
+import Projects from  "./components/Projects";
 
 function App() {
   return (
     <main className="text-gray-200 bg-gray-800">
-      
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
 
 
     </main>
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,9 +1,39 @@
-
-
+// import { ArrowRightIcon } from "@heroicons/react/solid";
+import React from "react";
+import GitHub from "../images/mark.png"
+import logo2 from "../images/Logo.png"
 
 
 function Navbar (){
+    return (
+        <header className="bg-gray-800 sm:sticky top-0 z-10">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col sm:flex-row  items-center">
+                <a className="title-font font-medium text-white mb-2 sm:mb-0">
+                        <a href="#about" className= "text-xl  hover:text-gray-400">
+                            About
+                        </a>
+                    </a>
+                    <nav className="title-font font-medium text-white mb-2 sm:mb-0">
+                        <a href="#projects" className="ml-4 text-xl  hover:text-gray-400">
+                            Projects
+                        </a>
+                        <a href="#Contact" className="ml-4 text-xl  hover:text-gray-400">
+                            Contact
+                        </a>
+                    </nav>
+                    <a href="https://github.com/ntraugh">
+                        <img src={GitHub} alt="GitHub" className="w-8 h-8 inline-flex hover:grow-2 ml-4 sm: mt-0"></img>
+                    </a>
+                    <a href="https://www.linkedin.com/in/nate-traugh-7bb18a20a/">
+                        <img src={logo2} alt="LinkedIn" className="w-100 h-8 inline-flex ml-4 hover:grow-2 sm: mt-0"></img>
+                    </a>
+            </div>
 
+
+
+
+        </header>
+    )
 }
 
 
